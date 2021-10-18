47f0d00071d9af1827f92b4c1e37c8794fbbd5e7
djcranney <danieljamescranney@gmail.com>
2021-10-18 10:48:05 +0100

    Minor typo corrected

--- pages/people-who-mother/cheddar-gorge.js
+++ pages/people-who-mother/cheddar-gorge.js
@@ -8,7 +8,7 @@ const CheddarGorge = () => {
         title={`Cheddar Gorge, Somerset`}
         bgName={`bg-cheddarGorge`}
         bgPosition={"bg-bottom"}
-        tagline={`Armenian girls do it closer to home (Silva Semerciyan)`}
+        tagline={`Armenian girls are kept closer to home (Silva Semerciyan)`}
         secondLine={`You’re so far away, doesn’t anybody stay in one place any more? (Carole King)`}
         text={`I wrote this with Armenian American playwright Silva Semerciyan in April 2021, as we came out of lockdown. Our conversations meandered between fleeing a genocide, escaping childcare duties to roam free in the landscape, and the acute awareness of distance between ourselves and our own parents during a time when it is not easy to get home, even when we want to.`}
       />
